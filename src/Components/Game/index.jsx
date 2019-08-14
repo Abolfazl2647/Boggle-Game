c0553a1099cda4fd250100f1a0f84b2492e9e5c2
abolfazl <abolfazl@arianatech.ir>
2019-08-15 04:00:48 +0430

    select cells on hover handled

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -6,13 +6,31 @@ export default class Game extends Component {
 	// constructor(props){
 	// 	super(props);
 	// }
+
+	state = {
+		string:"",
+		selectedCells:[]
+	}
+
+	handleMouseEnter(item) {
+		if ( !this.props.draging ) return;
+		let selected = [...this.state.selectedCells];
+			selected.push(item.id);
+		this.setState({selectedCells:selected});
+	}
+
 	render() {
 		console.log(this.props)
 		return (
 			<div className="Game-Wrapper">
 				<div className="Game">
 					{this.props.tableValues.map((item) => {
-						return <div className="cell" key={item.id}>{item.value}</div>
+						return <div 
+									className={((this.state.selectedCells.indexOf(item.id) !== -1 ? "active" : "") + " cell" )}
+									key={item.id}
+									onClick={()=> {this.handleMouseEnter(item)}}
+									onMouseEnter={()=> {this.handleMouseEnter(item)}}
+									>{item.value}</div>
 					})}
 				</div>
 			</div>
--- src/Components/Help/index.jsx
+++ src/Components/Help/index.jsx
@@ -5,14 +5,12 @@ export default class Modal extends Component {
 
     constructor(props){
         super(props);
-
         this.wrapperRef = React.createRef();
         this.handleClickOutside = this.handleClickOutside.bind(this);
     }
 
     componentWillUnmount() { document.removeEventListener('mousedown', this.handleClickOutside)}
     componentDidMount() {  document.addEventListener('mousedown', this.handleClickOutside)}
-
     handleClickOutside = (event) => {
         if ( this.props.visibility ) {
             if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
@@ -25,13 +23,10 @@ export default class Modal extends Component {
         return (
             <div className={(( this.props.visibility ) ? 'show' : "") + " Modal-Wrapper "}>
                 <div className="Modal" ref={this.wrapperRef}>
-                    <p>
-                        <i class="fa fa-tags" aria-hidden="true"></i>
-                        <span>راهنما</span>
-                    </p>
+                    <p><i className="fa fa-tags" aria-hidden="true"></i><span>راهنما</span></p>
                     <ul>
                         {this.props.Answers.map((item,index)=>{
-                            return <li key={index}><i class="fa fa-tag" aria-hidden="true"></i><span>{item}</span></li>
+                            return <li key={index}><i className="fa fa-tag" aria-hidden="true"></i><span>{item}</span></li>
                         })} 
                     </ul>
                 </div>
@@ -39,4 +34,3 @@ export default class Modal extends Component {
         );
     }
 }
-
